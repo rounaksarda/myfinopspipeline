56011fc55cc64b1849ed75876ec577882c5002d0
Rounak Sarda <36100122+rounaksarda@users.noreply.github.com>
2025-09-16 21:52:05 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+{
+  "name": "my-node-app",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "src/index.js",
+  "scripts": {
+    "lint": "eslint .",
+    "test": "jest",
+    "build": "echo 'Build step (add Babel or TypeScript here)'",
+    "start": "node src/index.js"
+  },
+  "devDependencies": {
+    "eslint": "^8.50.0",
+    "jest": "^29.0.0"
+  }
+}
